146a3dc9b5a9bb451ad2ff92ffc9eea788a2a05c
Mateusz Borkowski <m.borkowski.2005@gmail.com>
2024-05-13 15:09:20 +0200

    add csv to obj

--- data/data.mjs
+++ data/data.mjs
@@ -67,7 +67,7 @@ const getNewToken = (oAuth2Client) => {
       oAuth2Client.setCredentials(token);
       fs.writeFileSync(tokenPath,JSON.stringify(token));
       console.log("Token has been saved, refresh app");
-      exit();
+      return;
       updatedSpreedSheet(SPREAD_LINK.substring(39).split('/')[0]);
     })
   })
@@ -102,9 +102,63 @@ const refreshAccessToken = async () => {
   return data.access_token;
 }
 
+function csvToObjects(csv) {
+    const csvRows = csv.split("\n");
+    const propertyNames = csvSplit(csvRows[0]);
+    let objects = [];
+    for (let i = 1, max = csvRows.length; i < max; i++) {
+      let thisObject = {};
+      let row = csvSplit(csvRows[i]);
+      for (let j = 0, max = row.length; j < max; j++) {
+        thisObject[propertyNames[j]] = row[j];
+      }
+      objects.push(thisObject);
+    }
+    return objects;
+  }
+
+  function csvSplit(row) {
+    return row.split(",").map((val) => val.substring(1, val.length - 1));
+  }
+
+const getSpreedSheetToUpdated = async (SPREADSHEET_ID) => {
+    await refreshAccessToken().then( async (token)=>{
+        if(!token) return;
+        console.log('Pobieranie danych...');
+        // const linkToFetch = "https://sheets.googleapis.com/v4/spreadsheets/1XuCS4bhdBPXXeg_IQX3K4MZR8AHbH3FKjT03F59jYIw/values/'exchange-rates-log'!A1:AX?valueInputOption=USER_ENTERED";
+      
+        const linkToFetch = `https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID}/gviz/tq?tqx=out:csv&sheet=${SPREAD_SHEET}`;
+
+        const response = await fetch(
+            linkToFetch,
+            {
+              method: "GET",
+              headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+              },
+              
+              // body: SHEET_DATA
+            //   body: JSON.stringify({values: valuesData, insertDataOption: "insertDataOption"})
+            }
+          ).then(response=>response.text()).then((data)=>{
+            console.log(csvToObjects(data));
+            // console.log(data);
+            
+          })
+        
+        
+    })
+
+
+
+}
+
 const updatedSpreedSheet = async (SPREADSHEET_ID) => {
     console.log("SPREADSHEET_ID:",SPREADSHEET_ID);
- 
+
+    getSpreedSheetToUpdated(SPREADSHEET_ID);
+    return;
      await refreshAccessToken().then( async (token)=>{
       if(!token) return;
       console.log("Wprowadzanie danych...")
@@ -114,11 +168,13 @@ const updatedSpreedSheet = async (SPREADSHEET_ID) => {
             ["y", "x", 5],
         ]
 
-      const linkToFetch = SPREAD_SHEET=="0" ? 
-      `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/A1:AX?valueInputOption=USER_ENTERED`
-      : `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${SPREAD_SHEET!="" ? `'${SPREAD_SHEET}'!` : ""}A1:AX?valueInputOption=USER_ENTERED`;
+    //   const linkToFetch = SPREAD_SHEET=="0" ? 
+    //   `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/A1:AX?insertDataOption=INSERT_ROWS&valueInputOption=USER_ENTERED`
+    //   : `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${SPREAD_SHEET!="" ? `'${SPREAD_SHEET}'!` : ""}A1%3AAX:append?:append?insertDataOption=INSERT_ROWS&valueInputOption=USER_ENTERED`;
       
-      console.log("linkToFecth: ",linkToFetch)
+    //   console.log("linkToFecth: ",linkToFetch)
+
+        const linkToFetch = "https://sheets.googleapis.com/v4/spreadsheets/1XuCS4bhdBPXXeg_IQX3K4MZR8AHbH3FKjT03F59jYIw/values/'exchange-rates-log'!A1:AX?valueInputOption=USER_ENTERED";
 
       const response =  await fetch(
         linkToFetch,
@@ -129,7 +185,7 @@ const updatedSpreedSheet = async (SPREADSHEET_ID) => {
             'Content-Type': 'application/json'
           },
           // body: SHEET_DATA
-          body: JSON.stringify({values: valuesData})
+          body: JSON.stringify({values: valuesData, insertDataOption: "insertDataOption"})
         }
       )
     
